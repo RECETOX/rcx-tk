7b1fa98e415ec95ab18e9fe03a663623e50741ee
KristinaGomoryova <gomoryova@sci.muni.cz>
2024-06-13 15:18:59 +0200

    test for read_file successful

--- tests/test_process_metadata_file.py
+++ tests/test_process_metadata_file.py
@@ -6,17 +6,17 @@ from rcx_tk.process_metadata_file import read_file
 def test_read_file():
     d = {
         'File path': [
-            "Z:\000020-Shares\hram\MS_omics\Personal Folders\COUFALIKOVA Katerina\ATHLETE\finalni data zaloha\batch1-20231121-Katerina Coufalikova\RAW_profile\1_instrumental blank_01.raw",
-            "Z:\000020-Shares\hram\MS_omics\Personal Folders\COUFALIKOVA Katerina\ATHLETE\finalni data zaloha\batch1-20231121-Katerina Coufalikova\RAW_profile\4_Alkane mix_04.raw",
-            "Z:\000020-Shares\hram\MS_omics\Personal Folders\COUFALIKOVA Katerina\ATHLETE\finalni data zaloha\batch1-20231121-Katerina Coufalikova\RAW_profile\6_instrumental blank_06.raw",
-            "Z:\000020-Shares\hram\MS_omics\Personal Folders\COUFALIKOVA Katerina\ATHLETE\finalni data zaloha\batch1-20231121-Katerina Coufalikova\RAW_profile\7_procedural blank_07.raw",
-            "Z:\000020-Shares\hram\MS_omics\Personal Folders\COUFALIKOVA Katerina\ATHLETE\finalni data zaloha\batch1-20231121-Katerina Coufalikova\RAW_profile\8_QC non-dilute_08.raw",
-            "Z:\000020-Shares\hram\MS_omics\Personal Folders\COUFALIKOVA Katerina\ATHLETE\finalni data zaloha\batch1-20231121-Katerina Coufalikova\RAW_profile\11_QC 16_11.raw",
-            "Z:\000020-Shares\hram\MS_omics\Personal Folders\COUFALIKOVA Katerina\ATHLETE\finalni data zaloha\batch1-20231121-Katerina Coufalikova\RAW_profile\12_QC 8_12.raw",
-            "Z:\000020-Shares\hram\MS_omics\Personal Folders\COUFALIKOVA Katerina\ATHLETE\finalni data zaloha\batch1-20231121-Katerina Coufalikova\RAW_profile\15_QC non-dilute_15.raw",
-            "Z:\000020-Shares\hram\MS_omics\Personal Folders\COUFALIKOVA Katerina\ATHLETE\finalni data zaloha\batch1-20231121-Katerina Coufalikova\RAW_profile\18_QC 4 _18.raw",
-            "Z:\000020-Shares\hram\MS_omics\Personal Folders\COUFALIKOVA Katerina\ATHLETE\finalni data zaloha\batch1-20231121-Katerina Coufalikova\RAW_profile\19_QC 8_19.raw",
-            "Z:\000020-Shares\hram\MS_omics\Personal Folders\COUFALIKOVA Katerina\ATHLETE\finalni data zaloha\batch1-20231121-Katerina Coufalikova\RAW_profile\29_instrument blank_29.raw"
+            "Z:\\000020-Shares\\hram\\MS_omics\\Personal Folders\\COUFALIKOVA Katerina\\ATHLETE\\finalni data zaloha\\batch1-20231121-Katerina Coufalikova\\RAW_profile\\1_instrumental blank_01.raw",
+            "Z:\\000020-Shares\\hram\\MS_omics\\Personal Folders\\COUFALIKOVA Katerina\\ATHLETE\\finalni data zaloha\\batch1-20231121-Katerina Coufalikova\\RAW_profile\\4_Alkane mix_04.raw",
+            "Z:\\000020-Shares\\hram\\MS_omics\\Personal Folders\\COUFALIKOVA Katerina\\ATHLETE\\finalni data zaloha\\batch1-20231121-Katerina Coufalikova\\RAW_profile\\6_instrumental blank_06.raw",
+            "Z:\\000020-Shares\\hram\\MS_omics\\Personal Folders\\COUFALIKOVA Katerina\\ATHLETE\\finalni data zaloha\\batch1-20231121-Katerina Coufalikova\\RAW_profile\\7_procedural blank_07.raw",
+            "Z:\\000020-Shares\\hram\\MS_omics\\Personal Folders\\COUFALIKOVA Katerina\\ATHLETE\\finalni data zaloha\\batch1-20231121-Katerina Coufalikova\\RAW_profile\\8_QC non-dilute_08.raw",
+            "Z:\\000020-Shares\\hram\\MS_omics\\Personal Folders\\COUFALIKOVA Katerina\\ATHLETE\\finalni data zaloha\\batch1-20231121-Katerina Coufalikova\\RAW_profile\\11_QC 16_11.raw",
+            "Z:\\000020-Shares\\hram\\MS_omics\\Personal Folders\\COUFALIKOVA Katerina\\ATHLETE\\finalni data zaloha\\batch1-20231121-Katerina Coufalikova\\RAW_profile\\12_QC 8_12.raw",
+            "Z:\\000020-Shares\\hram\\MS_omics\\Personal Folders\\COUFALIKOVA Katerina\\ATHLETE\\finalni data zaloha\\batch1-20231121-Katerina Coufalikova\\RAW_profile\\15_QC non-dilute_15.raw",
+            "Z:\\000020-Shares\\hram\\MS_omics\\Personal Folders\\COUFALIKOVA Katerina\\ATHLETE\\finalni data zaloha\\batch1-20231121-Katerina Coufalikova\\RAW_profile\\18_QC 4 _18.raw",
+            "Z:\\000020-Shares\\hram\\MS_omics\\Personal Folders\\COUFALIKOVA Katerina\\ATHLETE\\finalni data zaloha\\batch1-20231121-Katerina Coufalikova\\RAW_profile\\19_QC 8_19.raw",
+            "Z:\\000020-Shares\\hram\\MS_omics\\Personal Folders\\COUFALIKOVA Katerina\\ATHLETE\\finalni data zaloha\\batch1-20231121-Katerina Coufalikova\\RAW_profile\\29_instrument blank_29.raw"
         ],
         'File name': [
             "1_instrumental blank_01",
@@ -43,11 +43,76 @@ def test_read_file():
             "QC",
             "QC",
             "Standard"
+       ],
+       "Class ID": [
+            3,
+            5,
+            3,
+            6,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            3
+       ],
+       "Batch": [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+       ],
+      "Analytical order": [
+            1,
+            4,
+            6,
+            7,
+            8,
+            11,
+            12,
+            15,
+            18,
+            19,
+            29
+      ],
+      "Inject. volume (µL)": [
+            6,
+            6,
+            6,
+            6,
+            6,
+            6,
+            6,
+            6,
+            6,
+            6,
+            6
+      ],
+       "Included": [
+            True,
+            True,
+            True,
+            True,
+            True,
+            True,
+            True,
+            True,
+            True,
+            True,
+            True
        ]
     }
 
     expected = pd.DataFrame(data = d)
-    file_path = os.path.join("test_data", "batch_specification1.xlsx")
-    #actual = read_file(file_path)
+    file_path = os.path.join("tests", "test_data", "batch_specification1.xlsx")
+    actual = read_file(file_path)
     #assert expected == actual
-    print(file_path)
+    assert actual.equals(expected)
