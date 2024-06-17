6f2d33332900427db006471a1ab6c91f346f9897
KristinaGomoryova <gomoryova@sci.muni.cz>
2024-06-17 11:23:24 +0200

    test for process_metadata_file with special characters included

--- tests/test_process_metadata_file.py
+++ tests/test_process_metadata_file.py
@@ -88,7 +88,7 @@ def dataframe():
             19,
             29
       ],
-      "Inject. volume (uL)": [
+      "Inject. volume (μL)": [
             6,
             6,
             6,
@@ -118,104 +118,7 @@ def dataframe():
 
     return pd.DataFrame(data = d)
 
-@pytest.fixture 
-def dataframe2():
-    d = {
-        'File name': [
-            "1_instrumental blank_01",
-            "4_Alkane mix_04",
-            "6_instrumental blank_06",
-            "7_procedural blank_07",
-            "8_QC non-dilute_08",
-            "11_QC 16_11",
-            "12_QC 8_12",
-            "15_QC non-dilute_15",
-            "18_QC 4 _18",
-            "19_QC 8_19",
-            "29_instrument blank_29"
-        ],
-       'Type': [
-            "Standard",
-            "Standard",
-            "Standard",
-            "Blank",
-            "QC",
-            "QC",
-            "QC",
-            "QC",
-            "QC",
-            "QC",
-            "Standard"
-       ],
-       "Class ID": [
-            3,
-            5,
-            3,
-            6,
-            2,
-            2,
-            2,
-            2,
-            2,
-            2,
-            3
-       ],
-       "Batch": [
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1
-       ],
-      "Analytical order": [
-            1,
-            4,
-            6,
-            7,
-            8,
-            11,
-            12,
-            15,
-            18,
-            19,
-            29
-      ],
-      "Inject. volume (uL)": [
-            6,
-            6,
-            6,
-            6,
-            6,
-            6,
-            6,
-            6,
-            6,
-            6,
-            6
-      ],
-       "Included": [
-            True,
-            True,
-            True,
-            True,
-            True,
-            True,
-            True,
-            True,
-            True,
-            True,
-            True
-       ]
-    }
-    
-    return pd.DataFrame(data = d)
- 
+
 @pytest.fixture 
 def processed_dataframe():
     d = {
