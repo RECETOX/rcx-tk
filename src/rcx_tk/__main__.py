0f9ca8269f977161ed222710be30c4c6729f4d1f
KristinaGomoryova <gomoryova@sci.muni.cz>
2024-06-20 16:12:51 +0200

    --help section for arguments added

--- src/rcx_tk/__main__.py
+++ src/rcx_tk/__main__.py
@@ -8,6 +8,13 @@ from rcx_tk.process_metadata_file import process_alkane_ri_file, process_metadat
 @click.argument('file_path')
 @click.argument('out_path')
 def main(method, file_path, out_path):
+    """Process metadata or alkane file.
+
+    Args:
+        method (string): A type of the file which is provided: a metadata file or an alkane file.
+        file_path (path): A path to the input data.
+        out_path (path): A path where the processed data will be exported to.
+    """
     if method == "metadata":
         process_metadata_file(file_path, out_path)
         click.echo("Metadata done!")
